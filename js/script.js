1934686a8cd4904285cd5379b3dcd0b4833600cb
Roman Tkachuk <96269607+TkachukRR@users.noreply.github.com>
2023-02-01 00:29:32 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,162 +3,178 @@
 const BASE_URL = "https://www.floatrates.com";
 
 //enter the currency code of the main country
-const BASIC_COUNTRY = `UAH`; 
+const BASIC_COUNTRY = `UAH`;
 
 //enter the bank commission
 const BANK_COMMISSION = 1.01;
 
-// enter the currency codes of the countries for currency exchange 
+// enter the currency codes of the countries for currency exchange
 // the first two currencies are displayed
-const userCountries = [ 'USD', 'EUR', 'PLN', 'CAD']; 
+const userCountries = ["USD", "EUR", "PLN", "CAD"];
 
 const refs = {
-    converter: document.querySelector('.converter'),
-    mainCours: document.querySelector('.converter__main_cours'),
-    selectFrom: document.querySelector('#selectFrom'),
-    courses: document.querySelector('.converter__courses'),
-    selectFrom: document.querySelector('#selectFrom'),
-    selectTo: document.querySelector('#selectTo'),
-    inputFrom: document.querySelector('.converter__courses_from__input'),
-    inputTo: document.querySelector('.converter__courses_to__input'),
-    flagBg: document.querySelector('.flag-bg'),
-} 
+  converter: document.querySelector(".converter"),
+  mainCours: document.querySelector(".converter__main_cours"),
+  selectFrom: document.querySelector("#selectFrom"),
+  courses: document.querySelector(".converter__courses"),
+  selectFrom: document.querySelector("#selectFrom"),
+  selectTo: document.querySelector("#selectTo"),
+  inputFrom: document.querySelector(".converter__courses_from__input"),
+  inputTo: document.querySelector(".converter__courses_to__input"),
+  flagBg: document.querySelector(".flag-bg"),
+};
 
 addBasicCountryToUsedCounties(BASIC_COUNTRY);
 
 makeApp();
 
 function addBasicCountryToUsedCounties(basicCountry) {
-    userCountries.unshift(basicCountry);
+  userCountries.unshift(basicCountry);
 }
 
 async function getCourses() {
-    try {
-        const dayliCourses = await fetchCourses();
-        // console.log(dayliCourses)
-        return dayliCourses;
-
-    } catch (e) {
-        console.error(e);
-    }
+  try {
+    const dayliCourses = await fetchCourses();
+    // console.log(dayliCourses)
+    return dayliCourses;
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 async function makeApp() {
-    const dayliCourses = await getCourses();
-    addMainCourseToPage(dayliCourses);
-    createCurrenciesElementsToPage(dayliCourses);
-    addSelectElementsToPage(dayliCourses);
-    addListeners(dayliCourses);
+  const dayliCourses = await getCourses();
+  addMainCourseToPage(dayliCourses);
+  createCurrenciesElementsToPage(dayliCourses);
+  addSelectElementsToPage(dayliCourses);
+  addListeners(dayliCourses);
 }
-
 
 async function fetchCourses() {
-    const url = BASE_URL;
-    const obj = {};
+  const url = BASE_URL;
+  const obj = {};
 
-    const arrayOfPromises = userCountries.map(async (userCountry) => {
-        const response = await fetch(`${url}/daily/${userCountry.toLowerCase()}.json`);
-        const data = await response.json();
-        obj[userCountry] = {
-                flagUrl: `https://countryflagsapi.com/svg/${userCountry.toLowerCase().slice(0, 2)}`,
-                ...data,
-            };
-    });
-    const courses = await Promise.all(arrayOfPromises);
-    return obj;
+  const arrayOfPromises = userCountries.map(async (userCountry) => {
+    const response = await fetch(
+      `${url}/daily/${userCountry.toLowerCase()}.json`
+    );
+    const data = await response.json();
+    obj[userCountry] = {
+      flagUrl: `./../images/${userCountry.toLowerCase().slice(0, 2) + ".svg"}`,
+      //   `https://countryflagsapi.com/svg/${userCountry.toLowerCase().slice(0, 2)}`,
+      ...data,
+    };
+  });
+  const courses = await Promise.all(arrayOfPromises);
+  return obj;
 }
-
 
 function addMainCourseToPage(obj) {
-    refs.mainCours.textContent = BASIC_COUNTRY;
-    refs.converter.style.backgroundImage = `url('${obj[BASIC_COUNTRY].flagUrl}')`;
+  refs.mainCours.textContent = BASIC_COUNTRY;
+  refs.converter.style.backgroundImage = `url('${obj[BASIC_COUNTRY].flagUrl}')`;
 }
 
-
 function createCurrenciesElementsToPage(obj) {
-    const setDisplayedCurrenciesCuntries = () => userCountries.slice(1,3);
-    const arrayOfDisplayedCurrencies = setDisplayedCurrenciesCuntries();
+  const setDisplayedCurrenciesCuntries = () => userCountries.slice(1, 3);
+  const arrayOfDisplayedCurrencies = setDisplayedCurrenciesCuntries();
 
-    for (let key of arrayOfDisplayedCurrencies) {
-        const markup = `
+  for (let key of arrayOfDisplayedCurrencies) {
+    const markup = `
             <li class="flex"> 
-                <h2 class="flag-bg" style="background-image: url('${obj[key].flagUrl}')">${key}</h2>
+                <h2 class="flag-bg" style="background-image: url('${
+                  obj[key].flagUrl
+                }')">${key}</h2>
                 <div class="converter__courses_value">
-                    <p>Sell ${obj[key][BASIC_COUNTRY.toLowerCase()].rate.toFixed(2)}</p>
-                    <p>Buy ${(obj[key][BASIC_COUNTRY.toLowerCase()].rate * BANK_COMMISSION).toFixed(2)}</p>
+                    <p>Sell ${obj[key][
+                      BASIC_COUNTRY.toLowerCase()
+                    ].rate.toFixed(2)}</p>
+                    <p>Buy ${(
+                      obj[key][BASIC_COUNTRY.toLowerCase()].rate *
+                      BANK_COMMISSION
+                    ).toFixed(2)}</p>
                 </div>
-            </li> `
-        refs.courses.insertAdjacentHTML("beforeend", markup);
-    }
+            </li> `;
+    refs.courses.insertAdjacentHTML("beforeend", markup);
+  }
 }
 
 function addSelectElementsToPage(obj) {
-    for (const element in obj) {
-        const markupSelect = `
+  for (const element in obj) {
+    const markupSelect = `
             <option value="${element}" >
                 ${element}
-            </option>`
-        refs.selectFrom.insertAdjacentHTML("beforeend", markupSelect);
-        refs.selectTo.insertAdjacentHTML("beforeend", markupSelect); 
-    }
+            </option>`;
+    refs.selectFrom.insertAdjacentHTML("beforeend", markupSelect);
+    refs.selectTo.insertAdjacentHTML("beforeend", markupSelect);
+  }
 
-    refs.selectFrom.value = userCountries[0];
-    refs.selectTo.value = userCountries[1];
+  refs.selectFrom.value = userCountries[0];
+  refs.selectTo.value = userCountries[1];
 }
 
 function addListeners(obj) {
-    let coefficient = 1;
-    refs.inputFrom.value = 0;
-    refs.inputTo.value = 0;
+  let coefficient = 1;
+  refs.inputFrom.value = 0;
+  refs.inputTo.value = 0;
 
-    const setCoefficient = () => {
-        if (refs.selectTo.value === refs.selectFrom.value) {
-            return coefficient = 1;
-        }
+  const setCoefficient = () => {
+    if (refs.selectTo.value === refs.selectFrom.value) {
+      return (coefficient = 1);
+    }
 
-        if (refs.selectFrom.value !== refs.selectTo.value) {
-            coefficient = obj[refs.selectFrom.value][refs.selectTo.value.toLowerCase()].rate;
-            
-            if (obj[refs.selectFrom.value][refs.selectTo.value.toLowerCase()].rate < obj[refs.selectTo.value][refs.selectFrom.value.toLowerCase()].rate) {
-                return coefficient /= BANK_COMMISSION;
-            }
-        };
-    };
+    if (refs.selectFrom.value !== refs.selectTo.value) {
+      coefficient =
+        obj[refs.selectFrom.value][refs.selectTo.value.toLowerCase()].rate;
 
-    const setCoefficient2 = () => {
-        if (refs.selectTo.value === refs.selectFrom.value) {
-            return coefficient = 1;
-        }
+      if (
+        obj[refs.selectFrom.value][refs.selectTo.value.toLowerCase()].rate <
+        obj[refs.selectTo.value][refs.selectFrom.value.toLowerCase()].rate
+      ) {
+        return (coefficient /= BANK_COMMISSION);
+      }
+    }
+  };
 
-        if (refs.selectFrom.value !== refs.selectTo.value) {
-            coefficient = obj[refs.selectTo.value][refs.selectFrom.value.toLowerCase()].rate;
-            
-            if (obj[refs.selectFrom.value][refs.selectTo.value.toLowerCase()].rate < obj[refs.selectTo.value][refs.selectFrom.value.toLowerCase()].rate) {
-                return coefficient *= BANK_COMMISSION;
-            }
-        };
-    };
+  const setCoefficient2 = () => {
+    if (refs.selectTo.value === refs.selectFrom.value) {
+      return (coefficient = 1);
+    }
 
-    const calculateFromInput = () => refs.inputTo.value = (refs.inputFrom.value * (coefficient)).toFixed(2);
-    const calculateFromInput2 = () => refs.inputFrom.value = (refs.inputTo.value * (coefficient)).toFixed(2);
+    if (refs.selectFrom.value !== refs.selectTo.value) {
+      coefficient =
+        obj[refs.selectTo.value][refs.selectFrom.value.toLowerCase()].rate;
 
-    refs.inputFrom.addEventListener('input', () => {
-        setCoefficient();
-        calculateFromInput();
-    });
+      if (
+        obj[refs.selectFrom.value][refs.selectTo.value.toLowerCase()].rate <
+        obj[refs.selectTo.value][refs.selectFrom.value.toLowerCase()].rate
+      ) {
+        return (coefficient *= BANK_COMMISSION);
+      }
+    }
+  };
 
-    refs.inputTo.addEventListener('input', () => {
-        setCoefficient2();
-        calculateFromInput2(refs.selectTo.value);
-    })
+  const calculateFromInput = () =>
+    (refs.inputTo.value = (refs.inputFrom.value * coefficient).toFixed(2));
+  const calculateFromInput2 = () =>
+    (refs.inputFrom.value = (refs.inputTo.value * coefficient).toFixed(2));
 
-    refs.selectFrom.addEventListener('change', () => {
-        setCoefficient();
-        calculateFromInput();
-    });
+  refs.inputFrom.addEventListener("input", () => {
+    setCoefficient();
+    calculateFromInput();
+  });
 
-    refs.selectTo.addEventListener('change', () => {
-        setCoefficient2();
-        calculateFromInput2();
-    });
+  refs.inputTo.addEventListener("input", () => {
+    setCoefficient2();
+    calculateFromInput2(refs.selectTo.value);
+  });
+
+  refs.selectFrom.addEventListener("change", () => {
+    setCoefficient();
+    calculateFromInput();
+  });
+
+  refs.selectTo.addEventListener("change", () => {
+    setCoefficient2();
+    calculateFromInput2();
+  });
 }
